bb644c6ba510dbb2a257df40a57734514fc169d7
UO283970 <98962779+UO283970@users.noreply.github.com>
2023-04-25 14:11:49 +0200

    SDI-2223-101-SW-11.1-Servicio-SOAP-Server.

---
+++ src/main/java/com/uniovi/sdi/Sdi2223105LabSoapSwServerApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223105LabSoapSwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223105LabSoapSwServerApplication.class, args);
+    }
+
+}
